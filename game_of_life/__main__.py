d66bc2aec423f094cb944fa9b52af43328aa8b4a
Felipe Moreno <anfmorenoso@unal.edu.co>
2021-07-12 01:35:25 +0200

    adding random initialization to main

--- game_of_life/__main__.py
+++ game_of_life/__main__.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from .grid import Grid
 import os
 
@@ -8,22 +9,33 @@ def ask_continue():
     return user_input
 
 
+def get_lines_cols_from_input() -> Tuple[int, int]:
+
+    print("Enter board size (Lines Columns):")
+    input_size = input()
+
+    while len(input_size.split(" ")) != 2:
+        print("invalid input size, try again, example : '10 15'")
+        input_size = input()
+
+    input_array = list(map(int, input_size.split(" ")))
+    os.system("cls")
+
+    return input_array[0], input_array[1]
+
+
 if __name__ == "__main__":
     os.system("cls")
-    world = Grid(
-        [
-            [0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0],
-        ]
-    )
-    world.print_initial_state()
+
+    n_lines, n_cols = get_lines_cols_from_input()
+    world = Grid.from_random_start(n_lines=n_lines, n_columns=n_cols)
+
+    print(world)
     user_input = ask_continue()
 
     while user_input != "n":
         os.system("cls")
         world.next_generation()
         print(world)
+        print(f"Generation #{world.n_generation}")
         user_input = ask_continue()
